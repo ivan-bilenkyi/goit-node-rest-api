dd35378b0f2a04c429fbe1fb31efb1bdc8401433
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2024-01-24 22:30:56 +0200

    fix Schema

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,24 +2,24 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "..", "db", "contacts.json"); //пас джоін обєднання шляху до поточного каталогу, папки і імелі файлу
+const contactsPath = path.join(__dirname, "..", "db", "contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8"); //parse сам робе ютф-8, можем не прописувати
+  const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 }
 
-async function getContactById(contactId) {
+async function getContactById(id) {
   const contacts = await listContacts();
-  const contact = contacts.find((item) => item.id === contactId);
+  const contact = contacts.find((item) => item.id === id);
   return contact || null;
 }
 
 async function removeContact(contactId) {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId); //пошук книжки яку треба видалити
-  if (index === -1) return null; //якщо книжку не знайшли повернули нал
-  const [result] = contacts.splice(index, 1); //повернувся масив з одним елементов видаленим, і записали в змінну
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) return null;
+  const [result] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return result;
 }
@@ -37,13 +37,18 @@ async function addContact(name, email, phone) {
   return newContact;
 }
 
-async function updateById(contactId, name, email, phone) {
+async function updateById(id, updatedItem) {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId); //пошук книжки яку треба видалити
-  if (index === -1) return null; //якщо книжку не знайшли повернули нал
-  contacts[index] = { id: contactId, name, email, phone };
+
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) return null;
+
+  const oldContact = contacts[index];
+  const updatedContact = { ...oldContact, ...updatedItem };
+  contacts[index] = updatedContact;
+
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contacts[index];
+  return updatedContact;
 }
 
 module.exports = {
